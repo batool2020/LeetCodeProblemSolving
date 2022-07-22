dc01864490d770869717305fa236213e2783737b
Seteve Jones <jones.wirebraincoffee@gmail.com>
2022-07-22 10:14:21 +0300

    Updating the file's position

---
+++ LeetCodeQuestions/LeetCodeQuestions/GoodPairs/GoodPairs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCodeQuestions
+{
+    public class GoodPairs
+    {
+
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i != j)
+                        count++;
+
+                }
+            }
+            return count/2;
+
+        }
+
+    }
+}

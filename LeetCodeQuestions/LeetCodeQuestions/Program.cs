6cf1bf0b885a5e3296580baf8729a7445ca2ec0a
Seteve Jones <jones.wirebraincoffee@gmail.com>
2022-07-22 10:15:32 +0300

    new files of the edited new project

---
+++ LeetCodeQuestions/LeetCodeQuestions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCodeQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 1, 1, 1 };
+            int result = GoodPairs.NumIdenticalPairs(array);
+            Console.WriteLine(result);
+
+            /* 
+             ReversedLinkedList Solution Driver
+             int[] array = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int k = RemoveDuplicatess(array);
+            Console.WriteLine(k);
+             */
+        }
+    }
+}

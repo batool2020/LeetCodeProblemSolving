c581c04b94c5be32411d522e136a829443b5bca6
Hiba Kurd <hal-kurd@restaurant365.com>
2022-08-18 13:29:32 +0300

    delete folder

--- GoodPairs/GoodPairs.cs
+++ GoodPairs/GoodPairs.cs
@@ -1,9 +1,24 @@
 ﻿using System;
-
-public class GoodPairs
+namespace LeetCodeQuestions
 {
-	public Class1()
-	{
+    public class GoodPairs
+    {
 
-	}
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i != j)
+                        count++;
+
+                }
+            }
+            return count/2;
+
+        }
+
+    }
 }
